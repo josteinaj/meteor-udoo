641edeb6bbcb3af975bab69726c1226c4d91ec10
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2014-09-14 18:42:19 +0200

    fixed rm mongod.lock

--- run-service.sh
+++ run-service.sh
@@ -5,13 +5,13 @@ set -e
 # * * * * * /home/debian/meteor-udoo/run-service.sh
 
 # change PROJECT_DIR to the path to your meteor project
-PROJECT_DIR="/home/debian/udoo-radio"
+PROJECT_DIR="/home/debian/Microscope"
 
 
 if [ "`ps aux | grep -v grep | grep bin/node | grep meteor | grep tools/main.js | wc -l`" = "0" ]; then
 
 cd $PROJECT_DIR
-if [ -f "./meteor/local/db/mongod.lock" ]; then rm "./meteor/local/db/mongod.lock" ; fi
+if [ -f "./.meteor/local/db/mongod.lock" ]; then rm "./.meteor/local/db/mongod.lock" ; fi
 authbind --deep meteor --port 80
 
 else
